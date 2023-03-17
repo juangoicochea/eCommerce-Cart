9a1ad719e51f03cd7d8a1ed7107f5346c3152198
Juan Carlos Goicochea <juancgoicochea@gmail.com>
2023-03-16 23:15:42 -0500

    Models and endpoints finished

---
+++ backend/controller/productControllers.js
@@ -0,0 +1,28 @@
+const { Product } = require('../config/db');
+
+const getAllProducts = async (req, res) => {
+    try {
+        const products = await Product.findAll({});
+
+        res.json(products);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: 'Server Error'});
+    }
+}
+
+const getProductById = async (req, res) => {
+    try {
+        const product = await Product.findByPk(req.params.id);
+
+        res.json(product);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: 'Server Error'});
+    }
+}
+
+module.exports = {
+    getAllProducts,
+    getProductById
+}
